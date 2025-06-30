1aacf2f93059d0287680c69aeacc54bf216b8b0d
Rusty Braze <180222500+RustyBraze@users.noreply.github.com>
2025-06-30 19:07:03 +0200

    Small fix - Spellcheck

--- config/config.default.php
+++ config/config.default.php
@@ -161,7 +161,7 @@ return [
             'navbar_classes' => 'navbar-dark',
         ],
         20 => [
-            'name' => 'Eurofurence 2024 - Cyberpunk ',
+            'name' => 'Eurofurence 2024 - Cyber punk ',
             'type' => 'dark',
             'navbar_classes' => 'navbar-dark',
         ],
