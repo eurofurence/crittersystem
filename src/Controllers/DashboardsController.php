27fcb7da955b2342ba03fa505241d39caa356930
kshade <kshade@crct.de>
2025-07-06 18:44:41 +0200

    Prepare for model

--- src/Controllers/DashboardsController.php
+++ src/Controllers/DashboardsController.php
@@ -5,23 +5,28 @@ declare(strict_types=1);
 namespace Engelsystem\Controllers;
 
 use Engelsystem\Config\Config;
-use Engelsystem\Helpers\Version;
 use Engelsystem\Http\Response;
 
 class DashboardsController extends BaseController
 {
-    public function __construct(protected Response $response, protected Config $config, protected Version $version)
-    {
+    public function __construct(
+        protected Response $response,
+        protected Config $config,
+    ) {
     }
 
-    public function index(): Response
+    public function showManagementDashboard(): Response
     {
         return $this->response->withView(
             'pages/dashboards/management.twig',
             [
                 'data' => 'something',
-                'version' => $this->version->getVersion(),
             ]
         );
+    }
+
+    public function index(): Response
+    {
+        return $this->showManagementDashboard();
     }
 }
