df5c87245ced43387629d5225b569e1f35c07141
kshade <kshade@crct.de>
2025-07-15 22:10:16 +0200

    pre merge

--- src/Controllers/DashboardsController.php
+++ src/Controllers/DashboardsController.php
@@ -5,22 +5,49 @@ declare(strict_types=1);
 namespace Engelsystem\Controllers;
 
 use Engelsystem\Config\Config;
+use Engelsystem\Helpers\Authenticator;
+use Engelsystem\Http\Exceptions\HttpForbidden;
 use Engelsystem\Http\Response;
+use Engelsystem\Models\AngelType;
+use Engelsystem\Models\Shifts\Shift;
+use Engelsystem\Models\Shifts\ShiftType;
+use Engelsystem\Models\Location;
+use Engelsystem\Models\User\User;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Support\Collection as SupportCollection;
 
 class DashboardsController extends BaseController
 {
+    /** @var array<string> */
+    protected array $permissions = [
+        'admin_shifts', // TODO: Give rights to managers
+    ];
+
     public function __construct(
+        private readonly Authenticator $auth,
         protected Response $response,
         protected Config $config,
+        protected AngelType $angelType,
+        protected Shift $shift,
+        protected ShiftType $shiftType,
+        protected User $user
     ) {
     }
 
+    // Renders the management dashboard, a condensed time table of all shifts & roles
     public function showManagementDashboard(): Response
     {
         return $this->response->withView(
             'pages/dashboards/management.twig',
             [
-                'data' => 'something',
+                'shiftTypes' => $this->shiftType->all(),
+                'shifts' => $this->shift->all(),
             ]
         );
     }
@@ -29,4 +56,5 @@ class DashboardsController extends BaseController
     {
         return $this->showManagementDashboard();
     }
+
 }
