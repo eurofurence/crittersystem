5c476ef004636604b91464e2f560e2c2001d1c8a
kshade <kshade@crct.de>
2025-07-16 07:50:03 +0200

    Added the big ugly query

--- src/Controllers/DashboardsController.php
+++ src/Controllers/DashboardsController.php
@@ -43,11 +43,41 @@ class DashboardsController extends BaseController
     // Renders the management dashboard, a condensed time table of all shifts & roles
     public function showManagementDashboard(): Response
     {
+
+        $query = $this->shift
+            ->join('shift_types', 'shifts.shift_type_id', '=', 'shift_types.id')
+            ->join('needed_angel_types', 'shifts.id', '=', 'needed_angel_types.shift_id')
+            ->join('angel_types', 'needed_angel_types.angel_type_id', '=', 'angel_types.id')
+            ->join('locations', 'shifts.location_id', '=', 'locations.id')
+            ->leftJoin('shift_entries', function($join){
+                $join
+                    ->on('needed_angel_types.shift_id', '=', 'shift_entries.shift_id')
+                    ->on('needed_angel_types.angel_type_id', '=', 'shift_entries.angel_type_id');
+            })
+            ->leftJoin('users', 'shift_entries.user_id', '=', 'users.id')
+            ->select('shifts.title AS shifts_title',
+                     'shifts.description AS shifts_description',
+                     'shifts.url AS shifts_url',
+                     'shifts.start AS shifts_start',
+                     'shifts.end AS shifts_end',
+                     'shifts.shift_type_id AS shifts_type_id',
+                     'shift_types.name AS shift_types_name',
+                     'shift_types.description AS shift_types_description',
+                     'needed_angel_types.count AS needed_angel_types_count',
+                     'angel_types.name AS angel_types_name',
+                     'angel_types.description AS angel_types_description',
+                     'locations.id AS locations_id',
+                     'locations.name AS locations_name',
+                     'locations.description AS locations_description',
+                     'users.id AS users_id',
+                     'users.name AS users_name')
+            ->get();
+
         return $this->response->withView(
             'pages/dashboards/management.twig',
             [
                 'shiftTypes' => $this->shiftType->all(),
-                'shifts' => $this->shift->all(),
+                'shifts' => $query,
             ]
         );
     }
@@ -56,5 +86,4 @@ class DashboardsController extends BaseController
     {
         return $this->showManagementDashboard();
     }
-
 }
