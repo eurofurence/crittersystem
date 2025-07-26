8952ff33559cdfefa44e6e319e8c5b6d754fc0fa
kshade <kshade@crct.de>
2025-07-26 20:26:58 +0200

    Fixed JS highlight

--- resources/assets/js/dashboards.js
+++ resources/assets/js/dashboards.js
@@ -9,12 +9,24 @@ ready(() => {
   dtables.forEach(function(element) {
     element.addEventListener('mouseover', (event) => {
       if (event.target.tagName !== 'SPAN') return true;
+      console.log(event.target.parentElement.dataset.critterId);
+      console.log(!event.target.parentElement.dataset.critterId);
       if (!event.target.parentElement.dataset.critterId) return true;
       document.querySelectorAll(
-        '[data-critter-id="${event.target.parentElement.dataset.critterId}"]').forEach(
-          (element) => element.classList.add("secondary-highlight") // TODO: Doesn't work
-        );
-    })
+        `[data-critter-id="${event.target.parentElement.dataset.critterId}"]`).forEach(
+          (element) => {
+            element.classList.add("secondary-highlight");
+          });
+    });
+    element.addEventListener('mouseout', (event) => {
+      if (event.target.tagName !== 'SPAN') return true;
+      if (!event.target.parentElement.dataset.critterId) return true;
+      document.querySelectorAll(
+        `[data-critter-id="${event.target.parentElement.dataset.critterId}"]`).forEach(
+          (element) => {
+            element.classList.remove("secondary-highlight");
+          });
+    });
   });
 
   // Highlight all shifts happening now
