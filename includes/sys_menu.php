9ac3f4f79a87b2445290c6d0b531c99456bfff93
Rusty Braze <180222500+RustyBraze@users.noreply.github.com>
2025-07-28 21:19:41 +0200

    Fix departments permission - menu view

--- includes/sys_menu.php
+++ includes/sys_menu.php
@@ -62,7 +62,7 @@ function make_navigation()
         'user_shifts'    => [__('general.shifts'), 'user_shifts'],
         'angeltypes'     => [__('angeltypes.angeltypes'), 'angeltypes'],
         'questions'      => [__('Ask the Info Desk'), 'question.add'],
-        'departments'    => __('departments.title.plural'),
+        'departments'    => [__('departments.title.plural'), 'dept.view'],
     ];
 
     foreach ($pages as $menu_page => $options) {
